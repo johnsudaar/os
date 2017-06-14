d4696fca06aff34c18704de8fa188f3dd8900b2f
Jonathan Hurter <john@scalingo.com>
2017-06-15 00:20:37 +0200

    First commit
    Some ASM reminders
    
    Signed-off-by: Jonathan Hurter <john@scalingo.com>

---
+++ tests_asm/bootloader.asm
@@ -0,0 +1,66 @@
+; -----------------
+; -  Hello worldi -
+; -----------------
+
+[org 0x7c00] ; Compensate for the BIOS padding (our program start at the 0x7c00 address
+
+; More interrupts documentation here: https://en.wikipedia.org/wiki/BIOS_interrupt_call
+
+mov ah, 0x0e ; ax register: 0x0eXX : (Print the al (XX) char) -> Write char in tty mode
+
+mov al, 'H' ; Storing the first char
+int 0x10 ; Call the interrupt (Video services)
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; Print l twice
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+
+mov bp, 0x8000 ; Load data far from our own code
+mov sp, bp     ; Initialize the Stack Pointer to BP
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+mov al, [0x7ffe] ; (0x8000 - 0x02 (we are in 16bit since we did'nt tell the processor otherwise) so this should be 'A')
+                 ; This is just here to prove that we could do show how bp and sp works (since A is in the two address 0x7fff and 0x7ffe)
+int 0x10
+
+mov al, ' '
+int 0x10
+
+mov bx, startup_message ; Storing the data into the bx register
+call print_string       ; Calling the print_string routine
+
+jmp $ ; Endless loop (Jum here)
+
+; Include dependencies
+%include "print_string.asm"
+
+; data
+startup_message:
+	db 'Booting JOSHUA OS v1.0', 0
+
+jmp $ ; Jump to current address
+
+; -----------------------------------
+; -  Padding and BIOS magic number  -
+; -----------------------------------
+times 510 - ($ - $$) db 0
+
+; Bios magic number
+dw 0xaa55
+
---
+++ tests_asm/print_string.asm
@@ -0,0 +1,19 @@
+print_string:           ; Take a string store in the BX register and print it
+	pusha 								; Store the current registry values
+	mov ah, 0x0e          ; Call 0x10 (0x03h) : TTY Write (BIOS)
+	jmp print_string_main ; Main print_string routine
+
+print_string_main:
+	mov cx, [bx]          ; Store the current value into the C register
+	cmp cl, 0             ; If the lower byte is 0
+	je print_string_end   ; Jump to the end
+
+	mov al, cl            ; Move the loweer byte into the lower al registry (TTY print)
+	int 0x10              ; Call the 0x10 instruction
+	add bx, 0x01          ; Advance the string pointer to the next byte
+	jmp print_string_main ; Jump to the next routine
+
+print_string_end:       ; print_string_end print_string cleanup
+	popa                  ; Pop the registry values
+	ret                   ; Return to the caller
+
